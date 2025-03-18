e0f172f9f9577df68db6ca9b6ef181c1f8722d65
Drawiel <daur0704@outlook.com>
2025-03-18 08:52:35 -0600

    Add project files.

---
+++ AromaCafeServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+
+namespace AromaCafeServer {
+    internal static class Program {
+        static void Main(string[] args) {
+            using (ServiceHost host = new ServiceHost(typeof(AromaCafeService.ServiceImplementation))) {
+                host.Open();
+                Console.WriteLine("Servidor iniciado");
+                Console.ReadLine();
+            }
+        }
+    }
+}
