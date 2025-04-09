59f18eb68e85f84ece16301878ddba7cc0412300
Daniel Uriel Alejandre Alvarado <daur0704@outlook.com>
2025-04-09 11:49:56 -0600

    Agregacion para cerrar una cuenta

---
+++ AromaCafeService/Models/Charge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AromaCafeService.Models {
+    [DataContract]
+    public class Charge {
+        [DataMember]
+        public int ChargeId { get; set; }
+        [DataMember]
+        public int TableId { get; set; }
+        [DataMember]
+        public decimal TotalCharge { get; set; }
+        [DataMember]
+        public string TypePayment { get; set; }
+        [DataMember]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ DataAccess/TableManagerDB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess {
+    public class TableManagerDB {
+        public static int ChargeBill(Cobro cobro) {
+            try {
+                using (var context = new AromaCafeBDEntities()) {
+                    var newCobro = new Cobro {
+                        idMesa = cobro.idMesa,
+                        TotalCobro = cobro.TotalCobro,
+                        Fecha = cobro.Fecha,
+                        TipoPago = cobro.TipoPago,
+                    };
+
+                    var gastoNuevo = context.Cobro.Add(newCobro);
+
+                    int result;
+                    return result = context.SaveChanges();
+
+                }
+            } catch (SqlException) {
+                return -1;
+            } catch (InvalidOperationException) {
+                return -1;
+            } catch (EntityException) {
+                return -1;
+            } catch (Exception) {
+                return -1;
+            }
+        }
+    }
+}
